2a88b87881b8b45ae751904ae1a79434ecc04b3a
ASZ <Andrzej.Szafarczyk@opitz-consulting.com>
2022-12-06 09:09:34 +0100

    Remove files tracked before gitignore

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\asz\Desktop\projekty\Cheese-Kettle\build\core\libb64\cdecode.c.o: \
- C:\Users\asz\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cdecode.c \
- C:\Users\asz\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cdecode.h \
- C:\Users\asz\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/stdint.h \
- C:\Users\asz\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\asz\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/sys/features.h \
- C:\Users\asz\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/sys/_intsup.h
